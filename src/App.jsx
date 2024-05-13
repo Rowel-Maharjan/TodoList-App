ca0ca2490da7ee2066cb7b3c50776874b0396d6b
Rowel Maharjan <rowelmhj@gmail.com>
2024-05-13 15:56:47 +0545

    Starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="container">
-        <div className='bg-red-800'>I am red</div>
-      </div>
+
     </>
   )
 }
