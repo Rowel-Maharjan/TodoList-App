db031cda44159091706ad6adcb6fb3e552328f24
Rowel Maharjan <rowelmhj@gmail.com>
2024-05-15 07:08:20 +0545

    Delete function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
 
-  
+
   const [todo, settodo] = useState("")
   const [todos, settodos] = useState([])
-  
+
   const handleEdit = () => {
-    
+
   }
-  const handleDelete = () => {
-    
+  const handleDelete = (e, id) => {
+    let newTodo = todos.filter(item => {
+      return item.id !== id
+    })
+    settodos(newTodo)
   }
   const handleAdd = () => {
     settodos([...todos, { id: uuidv4(), todo, isCompleted: false }])
@@ -35,13 +37,15 @@ function App() {
   //   newTodos[index].isCompleted = !newTodos[index].isCompleted
 
   //   settodos(newTodos)
-    
+
   // }
 
-  const handleCheckbox = (e)=>{
+
+
+  const handleCheckbox = (e) => {
     let id = e.target.name
-    let newTodo = todos.filter(item=>{
-      if(item.id === id){
+    let newTodo = todos.filter(item => {
+      if (item.id === id) {
         item.isCompleted = !item.isCompleted
       }
       return item
@@ -51,7 +55,7 @@ function App() {
 
   const handleChange = (e) => {
     settodo(e.target.value)
-  } 
+  }
 
   return (
     <>
@@ -60,11 +64,13 @@ function App() {
         <div className="addTodo">
           <h2 className='font-bold text-lg'>Add a Todo</h2>
           <input onChange={handleChange} value={todo} type="text" className='w-1/2 px-2 py-1 my-1 rounded-lg' />
-          <button  onClick={handleAdd} className='bg-blue-300 px-2 py-1 rounded-md mx-3 font-bold hover:bg-blue-400 '>Add</button>
+          <button onClick={handleAdd} className='bg-blue-300 px-2 py-1 rounded-md mx-3 font-bold hover:bg-blue-400 '>Save</button>
         </div>
 
         <h2 className='font-bold text-lg mt-4'>Your Todos</h2>
         <div className="todos max-h-[60vh] overflow-auto">
+
+          {todos.length == 0 && <div className='my-2'>-Add todo to display here.</div> }
 
           {todos.map(item => {
             return <div key={item.id} className="todo my-2">
@@ -75,7 +81,7 @@ function App() {
                 </div>
                 <div className="change flex gap-5">
                   <button onClick={handleEdit} className='bg-blue-300 px-2 py-1 rounded-md font-bold hover:bg-blue-400'>Edit</button>
-                  <button onClick={handleDelete} className='bg-blue-300 px-2 py-1 rounded-md font-bold hover:bg-blue-400 '>Delete</button>
+                  <button onClick={(e) => handleDelete(e, item.id)} className='bg-blue-300 px-2 py-1 rounded-md font-bold hover:bg-blue-400 '>Delete</button>
                 </div>
               </div>
             </div>
