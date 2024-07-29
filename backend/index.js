3932519d3e1ce8628914d2106a8b9857360dbd6c
Rowel Maharjan <rowelmhj@gmail.com>
2024-07-29 21:06:38 +0545

    FInal push

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 
-mongoose.connect('mongodb+srv://Rowel:oI09pDnFMlQhkHWp@cluster0.14jdqse.mongodb.net/Todo-list?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.14jdqse.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
 .then(()=>{
     console.log("Connection Successfull")
     app.listen(port, () => {
